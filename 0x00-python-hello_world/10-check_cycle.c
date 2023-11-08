7a82bbe226e2b86acbab5a45b3335d89e2cef713
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmail.com>
2023-11-08 11:30:05 -0800

    Task 10 - more efficient

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,25 +6,52 @@
  * check_cycle - a function in C that checks if a singly linked list has a cycle in it.
  * @list: pointer to head of a singly linked list
  *
+ * Description: Tis solution is more efficient.
  *Return: 0 if there is no cycle, 1 if there is a cycle.
  */
 
-int check_cycle(listint_t *list)
+int check_cycle(listint_t * list)
 {
-	int n, i = 0;
-	listint_t *current  = list, *head;
+	listint_t *slow, *fast;
+	slow = fast = list;
 
-	while (current != NULL)
+	while (slow && fast && fast->next)
 	{
-		head = list;
-		i++;
-		for (n = 0; n < i; n++)
-		{
-			if (current->next == head)
-				return (1);
-			head = head->next;
-		}
-		current = current->next;
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
 	}
 	return (0);
 }
+
+
+
+
+/**
+ * check_cycle - a function in C that checks if a singly linked list has a cycle in it.
+ * @list: pointer to head of a singly linked list
+ *
+ *Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+
+/*int check_cycle(listint_t *list)
+{
+        int n, i = 0;
+        listint_t *current  = list, *head;
+
+        while (current != NULL)
+        {
+                head = list;
+                i++;
+                for (n = 0; n < i; n++)
+                {
+                        if (current->next == head)
+                                return (1);
+                        head = head->next;
+                }
+                current = current->next;
+        }
+        return (0);
+}*/
+
--- 0x00-python-hello_world/10-main.c
+++ 0x00-python-hello_world/10-main.c
@@ -13,6 +13,7 @@ int main(void)
 	listint_t *head;
 	listint_t *current;
 	listint_t *temp;
+	listint_t *reset;
 	int i;
 
 	head = NULL;
@@ -23,31 +24,32 @@ int main(void)
 	add_nodeint(&head, 4);
 	add_nodeint(&head, 98);
 	add_nodeint(&head, 402);
+	add_nodeint(&head, 972);
 	add_nodeint(&head, 1024);
+	add_nodeint(&head, 2048);
 	print_listint(head);
 
-	if (check_cycle(head) == 0)
-		printf("Linked list has no cycle\n");
-	else if (check_cycle(head) == 1)
-		printf("Linked list has a cycle\n");
-
 	current = head;
-	for (i = 0; i < 4; i++)
+	for (i = 0; i < 6; i++)
+	{
+		if (i == 4)
+			temp = current;
 		current = current->next;
-	temp = current->next;
-	current->next = head;
-
-	if (check_cycle(head) == 0)
-		printf("Linked list has no cycle\n");
-	else if (check_cycle(head) == 1)
-		printf("Linked list has a cycle\n");
-
-	current = head;
-	for (i = 0; i < 4; i++)
-		current = current->next;
+	}
+	reset = current->next;
 	current->next = temp;
+
+	if (check_cycle(head) == 0)
+		printf("Linked list has no cycle\n");
+	else if (check_cycle(head) == 1)
+		printf("Linked list has a cycle\n");
+
+	current = head;
+	for (i = 0; i < 6; i++)
+		current = current->next;
+	current->next = reset;
 
 	free_listint(head);
 
 	return (0);
-} 
+}
